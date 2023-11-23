d0a9663baaf34aecdabe9865cdde03936576c849
Mchtsz <mhatland206@gmail.com>
2023-11-23 19:10:08 +0100

    Edit + delete works

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require("better-sqlite3")("database.db", { verbose: console.log });
+const db = require("better-sqlite3")("database.db");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 const app = express();
@@ -83,7 +83,49 @@ app.post("/addUser", (req, res) => {
   }
 });
 
+app.get("/admin/edit/user/:id", (req, res) => {
+  res.sendFile(__dirname + "/public/admin/edit/user/index.html");
+});
+
+app.post("/post/slettBruker/:id", (req, res) => {
+  const id = req.params.id;
+  const deleteStatement = db.prepare("DELETE FROM users WHERE id = ?");
+  deleteStatement.run(id);
+  res.redirect("/admin/edit");
+});
+
+app.post("/post/redigerBruker", (req, res) => {
+  const { id, name, email, rolle } = req.body;
+
+  const selectStatement = db.prepare("SELECT * FROM users WHERE id = ?");
+  const user = selectStatement.get(id);
+
+  if (name != user.name) {
+    const updateStatement = db.prepare(
+      "UPDATE users SET name = ? WHERE id = ?"
+    );
+    updateStatement.run(name, id);
+  }
+
+  if (email != user.email) {
+    const updateStatement = db.prepare(
+      "UPDATE users SET email = ? WHERE id = ?"
+    );
+    updateStatement.run(email, id);
+  }
+
+  if (rolle != user.rolle) {
+    if (rolle != "velg") {
+      const updateStatement = db.prepare(
+        "UPDATE users SET rolle = ? WHERE id = ?"
+      );
+      updateStatement.run(rolle, id);
+    }
+  }
+
+  res.redirect("/admin/edit");
+});
+
 app.listen(3000, () => {
-  console.log(`Server running on port 3000
-  `);
+  console.log(`Server running on port 3000`);
 });
