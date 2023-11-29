89ec980eb0284b1e4c019a286d1a7a3844c0cbb4
Mchtsz <mhatland206@gmail.com>
2023-11-29 11:57:54 +0100

    Did some small changes with updating user info

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
-import express from "express"
-import Database from "better-sqlite3"
-import bcrypt from "bcrypt" 
-import bodyParser from "body-parser"
-import cookieParser from "cookie-parser"
-import session from "express-session"
-import path from "path"
-import crypto from "crypto"
+import express from "express";
+import Database from "better-sqlite3";
+import bcrypt from "bcrypt";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import path from "path";
+import crypto from "crypto";
 
 const app = express();
 const db = new Database("database.db");
@@ -36,7 +36,9 @@ app.get("/json/users", (req, res) => {
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
-  const user = db.prepare("SELECT * FROM users WHERE email = ?").get(email) as any
+  const user = db
+    .prepare("SELECT * FROM users WHERE email = ?")
+    .get(email) as any;
 
   if (!user) {
     res.status(401).send("Invalid email or password");
@@ -46,10 +48,13 @@ app.post("/login", (req, res) => {
   const compare = bcrypt.compareSync(password, user.password);
 
   if (compare) {
-    res.cookie("token", user.token, { maxAge: 1000*60*60*24*7, httpOnly: true });
+    res.cookie("token", user.token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
     switch (user.rolle) {
       case "admin":
-        res.redirect("/admin/edit/");
+        res.redirect("/admin/");
         break;
       case "leder":
         res.redirect(`/leder/`);
@@ -61,7 +66,6 @@ app.post("/login", (req, res) => {
         res.redirect("/");
         break;
     }
-
   } else {
     res.status(401).send("Invalid email or password");
   }
@@ -70,7 +74,7 @@ app.post("/login", (req, res) => {
 app.post("/addUser", (req, res) => {
   const { name, email, rolle, password } = req.body;
   const user = db.prepare("SELECT * FROM users WHERE email = ?").get(email);
-  const token = crypto.randomUUID()
+  const token = crypto.randomUUID();
 
   if (user) {
     res.status(409).send("Email already exists");
@@ -81,7 +85,7 @@ app.post("/addUser", (req, res) => {
       res.redirect("/admin/edit/");
     }, 1000);
   }
-}); 
+});
 
 app.post("/post/slettBruker/:id", (req, res) => {
   const id = req.params.id;
@@ -96,113 +100,114 @@ app.get("/admin/edit/user/:id", (req, res) => {
 
 app.get("/medlem/kontaktinfo/:token", (req, res) => {
   const token = req.params.token;
-  const user = findByTokenStmt.get(token) as any
-  res.send(user)
-})
+  const user = findByTokenStmt.get(token) as any;
+  res.send(user);
+});
 
 app.post("/post/redigerBruker", (req, res) => {
   const token = req.cookies.token;
-  console.log(token)
-   const { id, name, email, rolle } = req.body;
+  console.log(token);
+  const { id, name, email, rolle } = req.body;
 
-  const user = findByTokenStmt.get(token) as any
+  const user = findByTokenStmt.get(token) as any;
 
   if (name != user.name) {
-    const updateStatement = db.prepare(
+    const updateStmt = db.prepare(
       "UPDATE users SET name = ? WHERE id = ?"
     );
-    updateStatement.run(name, id);
+    updateStmt.run(name, id);
   }
 
   if (email != user.email) {
-    const updateStatement = db.prepare(
+    const updateStmt = db.prepare(
       "UPDATE users SET email = ? WHERE id = ?"
     );
-    updateStatement.run(email, id);
+    updateStmt.run(email, id);
   }
 
   if (rolle != user.rolle) {
     if (rolle != "velg") {
-      const updateStatement = db.prepare(
+      const updateStmt = db.prepare(
         "UPDATE users SET rolle = ? WHERE id = ?"
       );
-      updateStatement.run(rolle, id);
+      updateStmt.run(rolle, id);
     }
-  } 
+  }
 
   res.redirect("/admin/edit");
 });
 
 app.get("/api/user/token", (req, res) => {
   const token = req.cookies.token;
-  const user = findByTokenStmt.get(token) as any
-  console.log(user, token)
+  const user = findByTokenStmt.get(token) as any;
+  console.log(user, token);
   if (user) {
     res.json({
       success: true,
-      data: user
-    })
+      data: user,
+    });
   } else {
     res.json({
-      success: false
-    })
+      success: false,
+    });
   }
-})
+});
 
 app.post("/post/redigerKontakt", (req, res) => {
   const token = req.cookies.token;
-  const user = findByTokenStmt.get(token) as any
+  const user = findByTokenStmt.get(token) as any;
 
   const { id, name, email, phone, birthdate, adress } = req.body;
 
   if (name != user.name) {
-    const updateStatement = db.prepare(
+    const updateStmt = db.prepare(
       "UPDATE users SET name = ? WHERE id = ?"
     );
-    updateStatement.run(name, id);
+    updateStmt.run(name, id);
   }
 
   if (email != user.email) {
-    const updateStatement = db.prepare(
+    const updateStmt = db.prepare(
       "UPDATE users SET email = ? WHERE id = ?"
     );
-    updateStatement.run(email, id);
+    updateStmt.run(email, id);
   }
 
   if (phone != user.phone) {
-    const updateStatement = db.prepare(
+    const updateStmt = db.prepare(
       "UPDATE users SET phone = ? WHERE id = ?"
     );
-    updateStatement.run(phone, id);
+    updateStmt.run(phone, id);
   }
 
   if (birthdate != user.birthdate) {
-    const updateStatement = db.prepare(
+    const updateStmt = db.prepare(
       "UPDATE users SET birthdate = ? WHERE id = ?"
     );
-    updateStatement.run(birthdate, id);
+    updateStmt.run(birthdate, id);
   }
 
   if (adress != user.adress) {
-    const updateStatement = db.prepare(
+    const updateStmt = db.prepare(
       "UPDATE users SET adress = ? WHERE id = ?"
     );
-    updateStatement.run(adress, id);
+    updateStmt.run(adress, id);
   }
+
   res.redirect("/medlem/");
-})
+});
 
 app.use((req, res, next) => {
-  let auth = ""
+  let auth = "";
 
   if (req.url.startsWith("/admin")) {
-    auth = "admin"
-  } else if (req.url.startsWith("/leder")) { 
-    auth = "leder"
+    auth = "admin";
+  } else if (req.url.startsWith("/leder")) {
+    auth = "leder";
   } else if (req.url.startsWith(`/medlem/`)) {
-    auth = "medlem"
+    auth = "medlem";
   } else {
-    return next()
+    return next();
   }
 
   const token = req.cookies.token;
@@ -211,7 +216,7 @@ app.use((req, res, next) => {
     res.redirect("/");
     return;
   }
-  const user = findByTokenStmt.get(token) as any
+  const user = findByTokenStmt.get(token) as any;
 
   if (user.rolle === auth) {
     next();
@@ -221,7 +226,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/", express.static(path.join(__dirname, "public")));
-
+app.use("/images", express.static(path.join(__dirname, "../images")));
 
 app.listen(3000, () => {
   console.log(`Server running on port 3000`);
