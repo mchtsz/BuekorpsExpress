dec60547ea793030371f68e87a66db49a506f4d8
Mchtsz <mhatland206@gmail.com>
2023-12-06 14:20:59 +0100

    optimized database, shortened code

--- testing.sql
+++ testing.sql
@@ -6,18 +6,14 @@ CREATE TABLE IF NOT EXISTS users (
     adress TEXT NOT NULL,
     birthdate TEXT NOT NULL,
     peletong_id INTEGER,
+    forelder_id INTEGER,
     email TEXT NOT NULL,
     password TEXT NOT NULL,
     token TEXT NOT NULL,
-    FOREIGN KEY(peletong_id) REFERENCES peletong(id)
+    FOREIGN KEY(peletong_id) REFERENCES peletong(id),
+    FOREIGN KEY(forelder_id) REFERENCES forelder(id)
 );
-CREATE TABLE IF NOT EXISTS posts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    user_id INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id)
-);
+
 CREATE TABLE IF NOT EXISTS kompani (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
@@ -33,12 +29,4 @@ CREATE TABLE IF NOT EXISTS forelder (
     email TEXT NOT NULL,
     password TEXT NOT NULL,
     token TEXT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS forelder (
-    forelder_id INTEGER NOT NULL,
-    medlem_id INTEGER NOT NULL,
-    PRIMARY KEY(forelder_id, medlem_id),
-    FOREIGN KEY(forelder) REFERENCES forelder(id),
-    FOREIGN KEY(medlem_id) REFERENCES users(id)
 );
